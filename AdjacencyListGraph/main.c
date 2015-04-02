b19c7819280422fdb410dc9949fd1269d5fe552f
annethf <annethf@outlook.com>
2015-04-02 15:59:59 +0800

    AdjacencyListGraph

--- AdjacencyListGraph/main.c
+++ AdjacencyListGraph/main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 
 #define MAX_VERTEX_NUM 20
+#define STACK_SIZE 100
+#define INCREASE 10
 
+//邻接表表示图
 typedef struct arcNode
 {
     int adjVex;
@@ -22,6 +25,120 @@ typedef struct
     int vexNum, arcNum;
 }adjListGraph;
 
+void (*visitFunc)(adjListGraph g, int);
+int visited[MAX_VERTEX_NUM];
+//栈定义
+typedef struct
+{
+    char* base, *top;
+    int stackSize;
+} stack;
+//队列定义
+typedef struct qNode
+{
+    char elem;
+    struct qNode *next;
+} queue;
+
+//栈操作
+void initStack(stack *s)
+{
+    s->base = (char*)malloc(sizeof(char) * STACK_SIZE);
+    s->top = s->base;
+    s->stackSize = STACK_SIZE;
+}
+
+void destroyStack(stack *s)
+{
+    free(s->top);
+    s->top = NULL;
+    free(s->base);
+    s->base = NULL;
+}
+
+int stackEmpty(stack s)
+{
+    return (s.base == s.top) ? 1 : 0;
+}
+
+void getTop(stack *s, char *elem)
+{
+    if(s->top == s->base) return;
+    *elem = *--s->top;
+    s->top++;
+}
+
+void push(stack *s, char elem)
+{
+    if(s->top - s->base >= s->stackSize)
+    {
+        s->base = (char*)realloc(s->base, sizeof(char) * (s->stackSize + INCREASE));
+        s->stackSize += INCREASE;
+        s->top = s->base + s->stackSize;
+    }
+    *s->top++ = elem;
+}
+
+void pop(stack *s, char *elem)
+{
+    if(s->base == s->top) return;
+    else
+        *elem = *--s->top;
+}
+
+//队列操作
+queue* initQueue(queue *q)
+{
+    q = (queue*)malloc(sizeof(queue));
+    q->elem = 0;
+    q->next = NULL;
+    return q;
+}
+
+void destroyQueue(queue *q)
+{
+    queue *p = q;
+    while(p->next)
+    {
+        q = p->next;
+        free(p);
+        p = q;
+    }
+    free(p);
+    q = p = NULL;
+}
+
+int queueEmpty(queue q)
+{
+    return (!q.next) ? 1 : 0;
+}
+
+void enQueue(queue *q, char elem)
+{
+    queue *node = (queue*)malloc(sizeof(queue));
+    node->elem = elem;
+    node->next = NULL;
+    queue *p = q;
+    if(!q->next)
+        q->next = node;
+    else
+    {
+        while(p->next)
+            p = p->next;
+        p->next = node;
+    }
+}
+
+void deQueue(queue *q, char *elem)
+{
+    queue *p = q->next;
+    q->next = p->next;
+    *elem = p->elem;
+    free(p);
+    p = NULL;
+}
+
+//邻接表表示图的操作
 int locateVex(adjListGraph graph, char data)
 {
     int i = 0;
@@ -126,6 +243,151 @@ void createALGraph(adjListGraph *graph)
     }
 }
 
+void visit(adjListGraph g, int v)
+{
+    printf("%c ", g.adjList[v].data);
+}
+
+int firstAdjVex(adjListGraph *g, int v)
+{
+    arcNode *p = g->adjList[v].firstArc;
+    if(p)
+        return p->adjVex;
+    return -1;
+}
+
+int nextAdjVex(adjListGraph *g, int v, int w)
+{
+    arcNode *p = g->adjList[v].firstArc;
+    while(p && p->adjVex <= w)
+        p = p->nextArc;
+    if(p)
+        return p->adjVex;
+    return -1;
+}
+
+void DFS(adjListGraph *g, int v, void (*visitFunc)(adjListGraph, int))
+{
+    visited[v] = 1;
+    (*visitFunc)(*g, v);
+    int w = firstAdjVex(g, v);
+    if(w < 0 || w >= g->vexNum) return;
+    else
+        for(; w >= 0; w = nextAdjVex(g, v, w))
+            if(!visited[w])
+                DFS(g, w, (*visitFunc));
+}
+
+void DFSTraverse(adjListGraph *g)
+{
+    visitFunc = visit;
+    int v = 0;
+    for(v = 0; v < g->vexNum; v++)
+        visited[v] = 0;
+    for(v = 0; v < g->vexNum; v++)
+        if(!visited[v])
+            DFS(g, v, (*visitFunc));
+}
+
+void print(char e)
+{
+    printf("%c ", e);
+}
+
+int findIndex(adjListGraph g, char elem)
+{
+    int i = 0;
+    for(i = 0; i < g.vexNum; i++)
+        if(g.adjList[i].data == elem)
+            return g.adjList[i].index;
+    return -1;
+}
+
+void DFSNonRecurTraverse(adjListGraph *g, void (*visitFunc)(char e))
+{
+    visitFunc = print;
+    int v = 0, w = 0;
+    char elem = 0;
+    for(v = 0; v < g->vexNum; v++)
+        visited[v] = 0;
+    stack *s = (stack*)malloc(sizeof(stack));
+    initStack(s);
+    for(v = 0; v < g->vexNum; v++)
+    {
+        if(!visited[v])
+        {
+            push(s, g->adjList[v].data);
+            (*visitFunc)(g->adjList[v].data);
+            visited[v] = 1;
+            v = firstAdjVex(g, v);
+        }
+        while(!stackEmpty(*s))
+        {
+            if(!visited[v])
+            {
+                push(s, g->adjList[v].data);
+                visited[v] = 1;
+                (*print)(g->adjList[v].data);
+                v = firstAdjVex(g, v);
+            }
+            else
+            {
+                getTop(s, &elem);
+                w = findIndex(*g, elem);
+                v = nextAdjVex(g, w, v);
+            }
+            if(v == -1)
+            {
+                pop(s, &elem);
+                w = findIndex(*g, elem);
+                v = nextAdjVex(g, w, v);
+            }
+        }
+    }
+    destroyStack(s);
+    s->stackSize = 0;
+    free(s);
+    s = NULL;
+}
+
+void BFSTraverse(adjListGraph *g, void (*visitFunc)(char e))
+{
+    visitFunc = print;
+    int v = 0, w = 0;
+    for(v = 0; v < g->vexNum; v++)
+        visited[v] = 0;
+    char elem;
+    queue *q = NULL;
+    q = initQueue(q);
+    for(v = 0; v < g->vexNum; v++)
+    {
+        if(!visited[v])
+        {
+            (*visitFunc)(g->adjList[v].data);
+            visited[v] = 1;
+            enQueue(q, g->adjList[v].data);
+        }
+        while(!queueEmpty(*q))
+        {
+            deQueue(q, &elem);
+            v = findIndex(*g, elem);
+            if(v != -1)
+            {
+                for(w = firstAdjVex(g, v); w >= 0; w = nextAdjVex(g, v, w))
+                {
+                    if(!visited[w])
+                    {
+                        visited[w] = 1;
+                        (*visitFunc)(g->adjList[w].data);
+                        enQueue(q, g->adjList[w].data);
+                    }
+                }
+            }
+        }
+    }
+    destroyQueue(q);
+}
+
 /*
 5 7
 ABCDE
@@ -136,6 +398,12 @@ int main()
     adjListGraph *graph = (adjListGraph*)malloc(sizeof(adjListGraph));
     createALGraph(graph);
     printALGraph(graph);
+    printf("DFS递归遍历：\n");
+    DFSTraverse(graph);
+    printf("\nDFS非递归遍历：\n");
+    DFSNonRecurTraverse(graph, print);
+    printf("\nBFS遍历：\n");
+    BFSTraverse(graph, print);
     destroyALGraph(graph);
     free(graph);
     graph = NULL;
